bbde67e7e8b89710d202ff83c1d9a8c15cd53742
fgolf <fontas.gkolf@gmail.com>
2020-04-11 19:01:21 +0300

    Fix stripped css selectors for share buttons

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,10 +49,10 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
+        develop: true, // Enable while using `gatsby develop` // todo remove when in production
         // tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // ignore: ['/src/components/share.module.css'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },

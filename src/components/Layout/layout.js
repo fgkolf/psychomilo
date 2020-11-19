09493af21e2e1459d71a3bf9e9fa953bbc7e0c60
fgkolf <fontas.gkolf@gmail.com>
2020-11-19 22:59:39 +0200

    Burger menu and navigation

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Link, graphql, useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import Navigation from './navigation';
 import BackToTop from './back-to-top';
-import useSiteTitle from '../../utils/helpers/useSiteTitle';
 import Hero from './hero';
 
 const useHero = () => {
@@ -28,23 +27,14 @@ const useHero = () => {
 
 const Layout = ({ children, location }) => {
   const hero = useHero();
-  const siteTitle = useSiteTitle();
-  const [toggleNav, setToggleNav] = React.useState(false);
-  const toggle = () => {
-    setToggleNav(!toggleNav);
-  };
   return (
     <div className="wrapper">
       <aside>
-        <Navigation toggle={toggle} pathname={location.pathname} />
+        <Navigation pathname={location.pathname} />
         <Hero data={hero} />
       </aside>
       <div>{children}</div>
       <BackToTop />
-      <footer>
-        &copy;
-        <Link to="/">{siteTitle}</Link>
-      </footer>
     </div>
   );
 };
--- src/components/Layout/navigation.js
+++ src/components/Layout/navigation.js
@@ -1,37 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'gatsby';
+import wave from '../../../static/images/wave.svg';
 
-const NavBurger = ({ toggle }) => (
-  <button className="nav-burger" type="button" onClick={toggle}>
-    <div className="hamburger hamburger--collapse" aria-label="Menu" role="button" aria-controls="navigation">
-      <div className="hamburger-box">
-        <div className="hamburger-inner" />
+const Navigation = ({ pathname }) => {
+  const [showNav, setShowNav] = React.useState(false);
+
+  useEffect(() => {
+    const html = document.querySelector('html');
+    if (showNav) {
+      html.style.overflow = 'hidden';
+    } else {
+      html.style.overflow = 'visible';
+    }
+  }, [showNav]);
+
+  const toggle = () => {
+    setShowNav((prevShow) => !prevShow);
+  };
+
+  if (!showNav) {
+    return (
+      <>
+        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+        <div
+          className="burger"
+          aria-label="Menu"
+          role="button"
+          aria-controls="navigation"
+          tabIndex={0}
+          onClick={toggle}
+        >
+          <span />
+          <span />
+          <span />
+        </div>
+      </>
+    );
+  }
+  return (
+    <div className="overlay" onClick={toggle}>
+      <nav>
+        <ul>
+          <li className={`nav ${pathname === '/' ? 'nav-current' : ''}`}>
+            <Link to="/">ΑΡΧΙΚΗ</Link>
+          </li>
+          <li className={`nav ${pathname.startsWith('/blog') ? 'nav-current' : ''}`}>
+            <Link to="/blog">BLOG</Link>
+          </li>
+          <li className={`nav ${pathname.startsWith('/about') ? 'nav-current' : ''}`}>
+            <Link to="/about">ABOUT</Link>
+          </li>
+          <li className={`nav ${pathname.startsWith('/contact') ? 'nav-current' : ''}`}>
+            <Link to="/contact">CONTACT</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="wave">
+        <img src={wave} />
       </div>
     </div>
-  </button>
-);
-
-// TODO: THIS IS HIDDEN
-const Navigation = ({ toggle, pathname }) => (
-  <div style={{ display: 'none' }}>
-    <NavBurger toggle={toggle} />
-    <nav>
-      <ul>
-        <li className={`nav ${pathname === '/' ? 'nav-current' : ''}`}>
-          <Link to="/">Αρχική</Link>
-        </li>
-        <li className={`nav ${pathname.startsWith('/blog') ? 'nav-current' : ''}`}>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li className={`nav ${pathname.startsWith('/about') ? 'nav-current' : ''}`}>
-          <Link to="/about">About</Link>
-        </li>
-        <li className={`nav ${pathname.startsWith('/contact') ? 'nav-current' : ''}`}>
-          <Link to="/contact">Επικοινωνία</Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+  );
+};
 
 export default Navigation;

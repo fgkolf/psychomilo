9a3a876b3c78c7b202b3220d6aa56c0eb19596a9
fgkolf <fontas.gkolf@gmail.com>
2020-12-05 20:08:12 +0200

    Contact made responsive

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layout/layout';
 import ContactForm from '../components/Contact/contact-form';
-import CommunicationDetails from '../components/Contact/communication-details';
 
 const Contact = () => (
   <Layout>
     <div className="contact-wrapper">
       <h1>Get in touch</h1>
       <ContactForm />
-      <br />
-      <CommunicationDetails />
     </div>
   </Layout>
 );
